61e7d653ea95d310b12e60cf667c155f7c98d604
KELW METZ <jw-phoenix_92678@hotmail.com>
2018-05-15 18:51:09 +0800

    built basic pages

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,22 @@
 const app = getApp()
 
 Page({
+
+  data: {
+    motto: 'Welcome to PenguinShare'
+  },
   getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo 
    this.setData({
       userInfo: e.detail.userInfo
+    })
+  },
+
+  loginbutton: function() {
+    app.globalData.userInfo = e.detail.userInfo 
+    wx.navigateTo({
+      url: "/pages/browse/browse",
     })
   }
 })
